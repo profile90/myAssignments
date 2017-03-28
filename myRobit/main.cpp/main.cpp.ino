2f6e98e4369e6d6e0b1f375ef52a1b5ffcecb97a
profile90 <15sagr@stockholmscience.se>
2017-03-28 15:43:52 +0200

    did thing

---
+++ myRobit/main.cpp/main.cpp.ino
@@ -0,0 +1,22 @@
+
+#define motorA1 0
+#define motorA2 1
+#define motorAPwm 5 
+#define motorB1 4
+#define motorB2 3
+#define motorBPwm 6
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
